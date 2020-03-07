0d8a3324a036ddd49b9392ed9547fc8f65e813e6
kleinesfilmroellchen <malu.bertsch@gmail.com>
2020-03-07 13:28:13 +0100

    project setup
    first time using gradle, just some hello world

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * The ConlangDB Database system.
+ * Depends on the JDK modules for SQL and HTTP.
+ * Provides an API for extensions to use.
+ */
+module klfr.conlangdb {
+	requires java.base;
+	requires java.sql;
+	requires java.net.http;
+}
